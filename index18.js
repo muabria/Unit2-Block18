0b7ecf7ef80ea5d2952696027a24e36d14b3228d
Brianna Russo <briannarusso@Briannas-Air.lan>
2023-08-10 21:07:01 -0400

    workshop assignment

---
+++ index18.js
@@ -0,0 +1,55 @@
+/*                          Multiplication
+                         
+
+            PseudoCode for 'Multiplation Functoion' Test : UNIT
+
+        - A function called "multiplication" that returns the product of the two input numbers.
+        - A function called "concatOdds" takes two arrays of integers as arguments. 
+        - Should return a single array that only contains the odd numbers 
+        - Expect the odd numbers in ascending order from both of the arrays.
+        - Expect each number combo INPUT to result true (number, string, and etc) or false
+        - Expect the function to return
+        - Expect an error if input wrong number
+        -         
+
+    Example: 
+        concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
+        ...should result in [-1, 1, 3, 9, 15]
+
+*/
+
+/*                      Shopping Cart Function Test
+                         
+
+             PseudoCode for 'Shopping Cart Function Test' : UNIT
+
+    
+        A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. 
+        They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+    
+        Think about the following; you may need to make assumptions or decisions about the prompt and how the feature should behave:
+
+        - What should happen if the cart is empty?
+        - What needs to be shown to the user at each step of check out?
+        - What behaviors of this feature does the prompt miss or gloss over?
+
+    Hint: Observe the shopping cart and checkout features of some popular websites to get some ideas!
+    Rubric
+
+            PseudoCode for Function Test 
+        
+        - Should have a Page Source called 'Cart' for Selected items wanted to Purchase
+        - Should have Price + Amount 
+        - Should have Sign up AD for Guest Users Option
+        - Expect a Login for Account Holder
+        - Should provide a selector of an amount for quantity on EACH Product in cart
+        - Expect a page source of a 'Shopping Cart' for guest and log in users
+        - Expect on bottom page source amount total of products
+        - 
+        - 
+        -
+        -
+        -
+
+*/
+   
